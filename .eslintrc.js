c17d7bd83e07640af610156eeac529bd15347f14
plhnk <paul.hanaoka@gmail.com>
2024-06-10 12:26:39 -0700

    scaffold app, get to first milestone (page up, scripts/styles, mostly working)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    env: {
+      browser: true,
+      es6: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    rules: {
+      // Add your custom rules here
+    },
+  };
+  
---
+++ scripts/give.js
@@ -0,0 +1,31 @@
+// onpage nav
+
+window.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.give-content > section');
+    console.log(sections, 'sections');
+    console.log('script loaded');
+  const navLinks = document.querySelectorAll('.onpage-nav a');
+    console.log(navLinks, 'nav links');
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+
+    let currentSection = '';
+
+    sections.forEach((section) => {
+        console.log(section.getAttribute('id'), section.offsetTop);
+
+      const sectionTop = section.offsetTop;
+      if (window.scrollY >= sectionTop - 150) {
+        // Adjust the offset as needed
+        currentSection = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === `#${currentSection}`) {
+        link.classList.add('active');
+      }
+    });
+  });
+});
